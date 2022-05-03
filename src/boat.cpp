f861172e9e1dc77ac2818cb1636d166f823fa1d4
Fabien Roger <efab.roger@gmail.com>
2022-05-03 10:23:05 +0200

    Start sail

--- src/boat.cpp
+++ src/boat.cpp
@@ -6,16 +6,24 @@
 #include <vector>
 
 using namespace cgp;
+using namespace std;
 
 void Boat::initialize()
 {
 	mesh boatMesh = mesh_load_file_obj("assets/boat.obj");
-	mesh_drawable.initialize(boatMesh, "boat mesh");
-	mesh_drawable.transform.scaling = .08f;
-	mesh_drawable.transform.translation = { -0,0,0.5f };
+	boat_mesh.initialize(boatMesh, "boat mesh");
+	boat_mesh.transform.scaling = .08f;
+	boat_mesh.transform.translation = { -0,0,0.5f };
+
+	sailPositions.resize(nbVertical, vector<vec3>(nbHorizontal));
 }
 
 void Boat::draw(cgp::scene_environment_basic_camera_spherical_coords& env)
 {
-	cgp::draw(mesh_drawable, env);
+	cgp::draw(boat_mesh, env);
+}
+
+void Boat::update_sail()
+{
+
 }
