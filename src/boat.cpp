e2ea65d685a3e5a73e6d2c603818ff10827db14d
Fabien Roger <efab.roger@gmail.com>
2022-05-10 09:50:50 +0200

    Add sail

--- src/boat.cpp
+++ src/boat.cpp
@@ -4,6 +4,7 @@
 #include "utils.hpp"
 
 #include <vector>
+#include<iostream>
 
 using namespace cgp;
 using namespace std;
@@ -14,13 +15,18 @@ void Boat::initialize()
 	boat_mesh.initialize(boatMesh, "boat mesh");
 	boat_mesh.transform.scaling = .08f;
 
-	sailPositions.resize(nbVertical, vector<vec3>(nbHorizontal));
+	generateStartSail();
+	sail_mesh_drawable.initialize(sail_mesh, "sail");
+	sail_mesh_drawable.shading.color = { 1.0f,1.0f,1.0f };
+	sail_mesh_drawable.shading.phong.specular = 0.3f;
 }
 
 void Boat::draw(cgp::scene_environment_basic_camera_spherical_coords& env)
 {
 	boat_mesh.transform.translation = { position.x, position.y,position.z };
 	cgp::draw(boat_mesh, env);
+	cgp::draw(sail_mesh_drawable, env);
+	update_sail();
 }
 
 void Boat::update(Terrain& terrain){
@@ -29,9 +35,141 @@ void Boat::update(Terrain& terrain){
 	speed += vec3(0, 0, - g);
 	speed *= 1 - friction;
 	position += speed * dt;
+
+	update_sail();
+}
+
+void Boat::generateStartSail() {
+	int nbPoints = 0;
+	sailPositions.resize(nbVertical);
+	sailSpeeds.resize(nbVertical);
+	for (int i = 0; i < nbVertical; i++)
+	{
+		sailPositions[i] = vector<vec3>(nbHorizontal - i); // Triangular sail
+		sailSpeeds[i] = vector<vec3>(nbHorizontal - i);
+		for (int j = 0; j < nbHorizontal - i; j++)
+		{
+			vec3 verticalPos = i * sailHighPos / (nbVertical - 1);
+			vec3 horizontalPos = j * sailEndPos / (nbHorizontal - 1);
+			sailPositions[i][j] = sailLowPos + verticalPos + horizontalPos;
+			nbPoints++;
+		}
+	}
+
+	sail_mesh.position.resize(nbPoints);
+	int point = 0;
+	for (int i = 0; i < nbVertical; i++)
+	{
+		for (int j = 0; j < nbHorizontal - i; j++)
+		{
+			sail_mesh.position[point] = sailPositions[i][j];
+			point++;
+		}
+	}
+
+	//Création des triangles:
+	point = -1;
+	for (int i = 0; i < nbVertical; i++)
+	{
+		for (int j = 0; j < nbHorizontal - i; j++)
+		{
+			point++;
+			if (i == 0)continue;
+			if (j == nbHorizontal - i - 1) {
+				sail_mesh.connectivity.push_back(
+					{ point, point - (nbHorizontal - i), point - (nbHorizontal - i) - 1 }
+				);
+				sail_mesh.connectivity.push_back(
+					{ point, point - (nbHorizontal - i) -1, point - (nbHorizontal - i)}
+				);
+			}
+			if (j > 0) {
+				sail_mesh.connectivity.push_back(
+					{ point, point - (nbHorizontal - i) - 2, point - (nbHorizontal - i) - 1 }
+				);
+				sail_mesh.connectivity.push_back(
+					{ point, point - (nbHorizontal - i) - 1, point - (nbHorizontal - i) - 2 }
+				);
+
+				sail_mesh.connectivity.push_back(
+					{ point, point - 1, point - (nbHorizontal - i) - 2 }
+				);
+				sail_mesh.connectivity.push_back(
+					{ point, point - (nbHorizontal - i) - 2, point - 1 }
+				);
+			}
+		}
+	}
+
+
+	sail_mesh.fill_empty_field();
 }
 
 void Boat::update_sail()
 {
 
+	for (int i = 0; i < nbVertical; i++)
+	{
+		for (int j = 0; j < nbHorizontal - i; j++)
+		{
+			sailSpeeds[i][j] += wind;
+			vec3 idealPosition = vec3(0, 0, 0);
+
+			if (i > 0 && i < nbVertical - 1 && j > 0 && j < nbHorizontal - i - 1) {
+				idealPosition += sailPositions[i - 1][j];
+				idealPosition += sailPositions[i + 1][j];
+				idealPosition += sailPositions[i][j - 1];
+				idealPosition += sailPositions[i][j + 1];
+				idealPosition /= 4;
+			}
+			// diagonal
+			else if (j == nbHorizontal - i - 1 && i > 0 && i < nbVertical - 1 && j > 0) {
+				idealPosition += sailPositions[i - 1][j + 1];
+				idealPosition += sailPositions[i + 1][j - 1];
+				idealPosition /= 2;
+			}
+			// mat
+			else if (i == 0 && i < nbVertical - 1 && j > 0 && j < nbHorizontal - i - 1) {
+				idealPosition += sailPositions[i][j - 1];
+				idealPosition += sailPositions[i][j + 1];
+				idealPosition /= 2;
+			}
+			// bas
+			else if (i > 0 && i < nbVertical - 1 && j == 0 && j < nbHorizontal - i - 1) {
+				idealPosition += sailPositions[i - 1][j];
+				idealPosition += sailPositions[i + 1][j];
+				idealPosition /= 2;
+			}
+
+			sailSpeeds[i][j] += kSailSpring * (idealPosition - sailPositions[i][j]);
+
+			sailSpeeds[i][j] *= 1 - sailFriction;
+		}
+	}
+
+	for (int i = 0; i < nbVertical; i++)
+	{
+		for (int j = 0; j < nbHorizontal - i; j++)
+		{
+			sailPositions[i][j] += dt * sailSpeeds[i][j];
+		}
+	}
+
+	sailPositions[0][0] = sailLowPos;
+	sailPositions[nbVertical - 1][0] = sailHighPos + sailLowPos;
+	sailPositions[0][nbHorizontal - 1] = sailEndPos + sailLowPos;
+
+	int point = 0;
+	for (int i = 0; i < nbVertical; i++)
+	{
+		for (int j = 0; j < nbHorizontal - i; j++)
+		{
+			sail_mesh.position[point] = sailPositions[i][j] + position;
+			point++;
+		}
+	}
+
+	sail_mesh.compute_normal();//Ne pas oublier les normales du mesh évoluent - PI
+	sail_mesh_drawable.update_position(sail_mesh.position);//Mise à jour des positions - PI
+	sail_mesh_drawable.update_normal(sail_mesh.normal);//et des normales. - PI
 }
--- src/boat.hpp
+++ src/boat.hpp
@@ -10,22 +10,37 @@ using namespace cgp;
 
 class Boat {
 public:
-	int nbVertical = 5;
-	int nbHorizontal = 5;
-
+	// Boat position parameters
 	float heightAboveWater = 0;
-	vec3 position = vec3(0,0,0);
-	vec3 speed = vec3(0, 0, 0);
 	float kSpring = 0.1;
 	float dt = 1;
 	float friction = 0.1;
 	float g = 0.03;
 
+	vec3 position = vec3(0, 0, 0);
+	vec3 speed = vec3(0, 0, 0);
+
+	// Sait parameters
+	int nbVertical = 10;
+	int nbHorizontal = 10;
+	vec3 wind = vec3(0, 0.05, 0);
+	float sailFriction = 0.08;
+	float kSailSpring = 1.3;
+
+	vec3 sailLowPos = vec3(- 1.2f,0.0f,1.2f);
+	vec3 sailHighPos = vec3(1.0f, 0.0f,2.7f); // relative to low pos
+	vec3 sailEndPos = vec3(- 3.0f, 0.0f, 0.5f); // relative to low pos
+
+	vector<vector<vec3>> sailPositions;
+	vector<vector<vec3>> sailSpeeds;
+	cgp::mesh_drawable sail_mesh_drawable;
+	cgp::mesh sail_mesh;
+
 	cgp::timer_basic timer;
 	cgp::mesh_drawable boat_mesh;
-	vector<vector<vec3>> sailPositions;
 	void initialize();
 	void draw(cgp::scene_environment_basic_camera_spherical_coords& env);
 	void update(Terrain& terrain);
 	void update_sail();
+	void generateStartSail();
 };
