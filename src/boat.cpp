2bb8ec27b205afd026a6141c49338b97e168c0a0
Fabien Roger <efab.roger@gmail.com>
2022-05-10 08:37:01 +0200

    Add spring to boat

--- src/boat.cpp
+++ src/boat.cpp
@@ -13,14 +13,22 @@ void Boat::initialize()
 	mesh boatMesh = mesh_load_file_obj("assets/boat.obj");
 	boat_mesh.initialize(boatMesh, "boat mesh");
 	boat_mesh.transform.scaling = .08f;
-	boat_mesh.transform.translation = { -0,0,0.5f };
 
 	sailPositions.resize(nbVertical, vector<vec3>(nbHorizontal));
 }
 
 void Boat::draw(cgp::scene_environment_basic_camera_spherical_coords& env)
 {
+	boat_mesh.transform.translation = { position.x, position.y,position.z };
 	cgp::draw(boat_mesh, env);
+}
+
+void Boat::update(Terrain& terrain){
+	float desiredZ = heightAboveWater + terrain.evaluate_terrain_height(position.x, position.y, terrain.timer.t);
+	speed += vec3(0, 0, std::max((desiredZ - position.z) * kSpring, 0.f));
+	speed += vec3(0, 0, - g);
+	speed *= 1 - friction;
+	position += speed * dt;
 }
 
 void Boat::update_sail()
--- src/boat.hpp
+++ src/boat.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "cgp/cgp.hpp"
+#include "terrain.hpp"
 
 using namespace std;
 using namespace cgp;
@@ -11,10 +12,20 @@ class Boat {
 public:
 	int nbVertical = 5;
 	int nbHorizontal = 5;
+
+	float heightAboveWater = 0;
+	vec3 position = vec3(0,0,0);
+	vec3 speed = vec3(0, 0, 0);
+	float kSpring = 0.1;
+	float dt = 1;
+	float friction = 0.1;
+	float g = 0.03;
+
 	cgp::timer_basic timer;
 	cgp::mesh_drawable boat_mesh;
 	vector<vector<vec3>> sailPositions;
 	void initialize();
 	void draw(cgp::scene_environment_basic_camera_spherical_coords& env);
+	void update(Terrain& terrain);
 	void update_sail();
 };
